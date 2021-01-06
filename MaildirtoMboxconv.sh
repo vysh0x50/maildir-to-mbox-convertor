51d900ed3fd9660e129447b8238ab546bf2d2e02
Vyshnavlal <sudoerspass@gmail.com>
2021-01-06 21:08:46 +0530

    Code rechecked

--- MaildirtoMboxconv.sh
+++ MaildirtoMboxconv.sh
@@ -1,64 +1,54 @@
 #!/bin/bash
-echo "Please enter the cPanel username: "
-read user
+echo "Enter the Email account: "
+read email_account
 echo
-echo "Please enter the domain of email account: "
-read domain_name
-echo
-echo "Please enter the username of email account: "
-read user_name
-echo
-echo "Please enter the mail directory you wish to export: "
+echo "Enter the mail directory you wish to export: "
 read mail_dir
 
-#checking the existence of cpanel user
-cd /var/cpanel/users
- for $user in *
- do
-    if [[ -d $user ]];then
-    echo $user "found !"
-    else
-    echo "The " $user " not found in the server ! Please enter a valid cPanel user "
+domain= echo $email_account | awk -F\@ '{print $2'} 
+user_name= echo $email_account | awk -F\@ '{print $1}'
+user= grep $domain /etc/userdomains | awk '{print $2}'
+
+if [-z $user];then
+    echo "cPanel User not found !"
     exit
- done
-
-#checking the existence of entered domain under mail folder in the respective user
-cd /home/$user/mail
-for $domain_name in *
-do 
-  if [[ -d $domain_name ]];then
-  echo $domain_name " found for the user " $user
-  else
-  echo "The " $domain_name " not found for the user " $user " Please enter a valid domain name "
-  exit
-done
-
-#checking the existence of email account
-cd /home/$user/mail/$domain_name
-for $user_name in *
-do
-    if [[ -d $user_name ]]; then
-    echo "The email account " $user_name"@"$domain_name " found !"
+else
+    cd /home/$user/mail/$domain
+    if [-d $user_name];then
+    echo "Email account $email_account found!"
     else
-    echo "The email account " $user_name"@"$domain_name " not found ! Please enter a valid username "
+    echo "Email account $email_account not found"
     exit
-done
+    fi
+fi
 
-#checking the existence of entered mail directory
-cd /home/$user/mail/$domain_name/$user_name
+arr=($(ls -a | grep -E "^\.[a-zA-Z0-9]"))
 possible_inputs= (inbox Inbox INBOX)
 if [[ " ${possible_inputs[@]} " =~ " $mail_dir " ]];then
 mkdir /home/$user/Mail_backup
-    echo "Mail directory found !"
-    echo "Converting cur to mbox format"
+    
+    echo "Converting cur to mbox"
     for file in find /home/$user/mail/$domain_name/$user_name/cur -type f
     do
-        cat $file | formail -A Date: &gt;&gt; /home/$user/Mail_backup/cur.mbox
+        cat $file | formail -A Date: >> /home/$user/Mail_backup/cur.mbox
     done
-    echo "converting new to mbox format"
-    for file in find /home/$user/mail/$domain_name/new -type f
+    echo "converting new to mbox"
+    for file in find /home/$user/mail/$domain_name/$user_name/new -type f
     do
-        cat $file | formail -A Date: &gt;&gt; /home/$user/Mail_backup/new.mbox
+        cat $file | formail -A Date: >> /home/$user/Mail_backup/new.mbox
     done
-    echo "Converted inbox to mbox format, please check the output in /home/$user/Mail_backup"
+    echo "Finished converting !"
+    exit
+elif [[ "${arr[@]" =~ "$mail_dir "]];then
+    echo "Converting $mail_dir to mbox"
+    for file in find /home/$user/mail/$domain_name/$user_name/$mail_dir -type f
+    do
+        cat $file | formail -A Date: >> /home/$user/Mail_backup/$mail_dir.mbox
+    done
+    echo "Finished converting"
 fi
+
+echo "Zipping the output"
+cd /home/$user
+zip -r mailBackup.zip Mail_backup
+echo "Zipped the backup, check /home/$user"
