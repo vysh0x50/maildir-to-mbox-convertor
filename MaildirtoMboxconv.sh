3edaa160445eb82b1cd30c5b61dc9095dbc700d5
Vyshnavlal <sudoerspass@gmail.com>
2021-01-09 22:48:04 +0530

    modified

--- MaildirtoMboxconv.sh
+++ MaildirtoMboxconv.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
-##############################################################################################
-#           Bash Script to convert Maildir to Mbox using formail in cPanel servers           #
-##############################################################################################
+#Bash Script to convert Maildir to Mbox using formail in cPanel servers
 
 echo "Enter the Email account: "
 read email_account
@@ -9,9 +7,9 @@ echo
 echo "Enter the mail folder to convert: "
 read mail_dir
 
-domain=`echo $email_account | awk -F\@ '{print $2'}`
-user_name=`echo $email_account | awk -F\@ '{print $1}'`
-user=`fgrep "$domain" /etc/userdomains | awk '{print $2}'`
+domain=echo $email_account | awk -F\@ '{print $2'}
+user_name=echo $email_account | awk -F\@ '{print $1}'
+user=fgrep "$domain" /etc/userdomains | awk '{print $2}' | head -1
 
 #checking the respective cPanel user exist or not
 if [ -z "$user" ];then
@@ -20,7 +18,7 @@ if [ -z "$user" ];then
     
 else
     cd /home/$user/mail/$domain
-    if [ -d $user_name ];then
+    if [ -d "$user_name" ];then
     echo "Email account $email_account found!"
     else
     echo "Email account $email_account not found"
@@ -37,20 +35,20 @@ possible_inputs=(inbox Inbox INBOX)
 mkdir /home/$user/Mail_backup
 if [[ " ${possible_inputs} " =~ " ${mail_dir} " ]]; then
     echo "Converting cur to mbox"
-    for file in find /home/$user/mail/$domain/$user_name/cur -type f
+    for file in `find /home/$user/mail/$domain/$user_name/cur -type f`
     do
         cat $file | formail -A Date: >> /home/$user/Mail_backup/cur.mbox
     done
     echo "converting new to mbox"
-    for file in find /home/$user/mail/$domain/$user_name/new -type f
+    for file in `find /home/$user/mail/$domain/$user_name/new -type f`
     do
         cat $file | formail -A Date: >> /home/$user/Mail_backup/new.mbox
     done
     echo "Finished converting !"
     
-elif [[ " ${directory_list} " =~ " ${mail_dir,,} " ]]; then
+elif [[ " ${directory_list[@],,} " =~ " ${mail_dir,,} " ]]; then
     echo "Converting $mail_dir to mbox"
-    for file in find /home/$user/mail/$domain/$user_name/$mail_dir -type f
+    for file in `find /home/$user/mail/$domain/$user_name/.$mail_dir -type f`
     do
         cat $file | formail -A Date: >> /home/$user/Mail_backup/$mail_dir.mbox
     done
@@ -62,4 +60,3 @@ echo "Creating a zip of the mail backup..."
 cd /home/$user
 zip -r mailBackup.zip Mail_backup
 echo "Zip file created and placed it in /home/"$user" as mailBackup.zip"
-rm -rf Mail_backup
